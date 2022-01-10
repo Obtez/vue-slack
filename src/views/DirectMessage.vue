644e420e9c204e36b7cbbfdc3b7e97f2f7ccf37d
Obtez <59846143+Obtez@users.noreply.github.com>
2022-01-10 18:07:48 +0100

    added quill editor

--- src/views/DirectMessage.vue
+++ src/views/DirectMessage.vue
@@ -4,6 +4,15 @@
     <DirectMessageTop userName="Slackbot" />
     <DirectMessageDivider />
     <DirectMessageItem userName="Slackbot" />
+    <div class="editor-container">
+    <quill-editor
+      v-if="state.showEditor"
+      v-model:value="state.content"
+      :options="state.editorOption"
+      :disabled="state.disabled"
+    />
+    <button @click="submitMessage">Send</button>
+    </div>
   </section>
 </template>
 
@@ -12,6 +21,8 @@ import DirectMessageHeader from '@/components/direct-message/DirectMessageHeader
 import DirectMessageTop from '@/components/direct-message/DirectMessageTop.vue';
 import DirectMessageDivider from '@/components/direct-message/DirectMessageDivider.vue';
 import DirectMessageItem from '@/components/direct-message/DirectMessageItem.vue';
+import { reactive } from '@vue/reactivity';
+import { quillEditor } from 'vue3-quill';
 
 export default {
   name: 'DirectMessage',
@@ -20,6 +31,26 @@ export default {
     DirectMessageTop,
     DirectMessageDivider,
     DirectMessageItem,
+    quillEditor,
+  },
+  setup() {
+    const state = reactive({
+      curTheme: 'snow',
+      showEditor: 'true',
+      content: '<p>Write your message here...</p>',
+      editorOption: {
+        placeholder: 'core',
+        modules: {},
+      },
+      disabled: false,
+    });
+    return { state };
+  },
+  methods: {
+    submitMessage() {
+      const message = this.state.content;
+      console.log(message);
+    },
   },
 };
 </script>
@@ -29,5 +60,9 @@ export default {
   .direct-message-wrapper {
     display: flex;
     flex-direction: column;
+  }
+
+  .editor-container {
+    margin: 0 1rem;
   }
 </style>
